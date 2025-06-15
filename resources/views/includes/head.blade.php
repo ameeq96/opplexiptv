5a9357432daeef80ce6b35874c12bcd909f1915a
ameeqkhan <ameeqkhan183@gmail.com>
2025-06-15 15:55:25 +0500

    sdffdsdffds

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,210 +1,77 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>@yield('title')</title>
-<link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/fav-icon.webp') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<style>
-    .xmobile-slider {
-        width: 100%;
-        max-width: 428px;
-        margin: auto;
-        position: relative;
-        overflow: hidden;
-    }
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/fav-icon.webp') }}">
 
-    input[type="radio"] {
-        display: none;
-    }
-
-    .xslide-frame {
-        width: 100%;
-        flex-shrink: 0;
-        position: relative;
-    }
-
-    .xslide-frame img {
-        width: 100%;
-        height: auto;
-    }
-
-    .xslide-text {
-        position: absolute;
-        bottom: 10px;
-        left: 10px;
-        right: 10px;
-        color: #fff;
-        padding: 10px;
-        border-radius: 6px;
-    }
-
-    .xslide-text h5 {
-        margin: 0 0 5px;
-        font-size: 1rem;
-        color: #fff;
-    }
-
-    .xslide-btn,
-    .xslide-text p {
-        color: #fff;
-        font-size: 0.85rem;
-    }
-
-    .xslide-text p {
-        margin: 0 0 10px;
-    }
-
-    .xslide-btn {
-        background: #df0303;
-        padding: 6px 12px;
-        border-radius: 4px;
-        text-decoration: none;
-    }
-
-    .xslide-dots {
-        text-align: center;
-        margin-top: 8px;
-    }
-
-    .xslide-wrapper {
-        display: flex;
-        transition: transform 0.6s ease-in-out;
-    }
-
-    .xslide-wrapper.autoplay-track {
-        animation: 10s ease-in-out infinite autoplaySlider;
-    }
-
-    @keyframes autoplaySlider {
-
-        0%,
-        100%,
-        20%,
-        75% {
-            transform: translateX(0);
-        }
-
-        25%,
-        45% {
-            transform: translateX(-100%);
-        }
-
-        50%,
-        70% {
-            transform: translateX(-200%);
-        }
-    }
-</style>
-<script>
-    window.addEventListener('load', () => {
-        setTimeout(() => {
-            document.getElementById('xslider-autoplay').classList.add('autoplay-track');
-        }, 3000);
-    });
-</script>
-<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" as="style"
-    onload="this.onload=null;this.rel='stylesheet'">
-
-<link rel="preload" href="{{ asset('css/style.css') }}" as="style">
-
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
-<noscript>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</noscript>
-
-<!-- Preload CSS files for faster load -->
-<link rel="preload" href="{{ asset('css/global.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/header.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/footer.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/font-awesome.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/flaticon.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/animate.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/owl.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/swiper.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/linearicons.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/jquery-ui.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/custom-animate.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/jquery.fancybox.min.css') }}" as="style">
-<link rel="preload" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}" as="style">
-<link rel="preload" as="image" href="images/background/pattern-4.webp" as="style">
-@if (!empty($displayMovies[0]['webp_image_url']))
-    <link rel="preload" as="image" href="{{ $displayMovies[0]['webp_image_url'] }}" />
-@endif
-
-<!-- Apply the CSS files -->
-<link rel="stylesheet" href="{{ asset('css/global.css') }}">
-<link rel="stylesheet" href="{{ asset('css/header.css') }}">
-<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-<link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
-<link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-<link rel="stylesheet" href="{{ asset('css/owl.css') }}">
-<link rel="stylesheet" href="{{ asset('css/swiper.css') }}">
-<link rel="stylesheet" href="{{ asset('css/linearicons.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-<link rel="stylesheet" href="{{ asset('css/custom-animate.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
-@if (!empty($movies[0]['webp_image_url']))
-    <link rel="stylesheet" as="image" href="{{ $movies[0]['webp_image_url'] }}" />
-@endif
-
-<noscript>
-    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/swiper.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/linearicons.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom-animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
     @if (!empty($movies[0]['webp_image_url']))
-        <link rel="stylesheet" as="image" href="{{ $movies[0]['webp_image_url'] }}" />
+        <link rel="preload" as="image" href="{{ $movies[0]['webp_image_url'] }}" fetchpriority="high">
     @endif
-</noscript>
 
+    <link rel="preload" href="{{ asset('css/style.css') }}" as="style">
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" as="style" crossorigin>
 
-<link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
-<link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="all">
 
-<script>
-    window.dataLayer = window.dataLayer || [];
+    @php
+        $nonCriticalStyles = [
+            'global.css', 'header.css', 'footer.css', 'font-awesome.css', 'flaticon.css',
+            'animate.css', 'owl.css', 'swiper.css', 'linearicons.css', 'jquery-ui.css',
+            'custom-animate.css', 'jquery.fancybox.min.css', 'jquery.mCustomScrollbar.min.css'
+        ];
+    @endphp
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    document.addEventListener("DOMContentLoaded", function() {
-        let hasGTMLoaded = false;
+    @foreach ($nonCriticalStyles as $style)
+        <link rel="preload" href="{{ asset("css/$style") }}" as="style">
+    @endforeach
 
-        function loadGTM() {
-            if (!hasGTMLoaded) {
-                hasGTMLoaded = true;
+    @foreach ($nonCriticalStyles as $style)
+        <link rel="stylesheet" href="{{ asset("css/$style") }}" media="all">
+    @endforeach
+
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}" media="all">
+    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}" media="all">
+
+    <noscript>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        @foreach ($nonCriticalStyles as $style)
+            <link rel="stylesheet" href="{{ asset("css/$style") }}">
+        @endforeach
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+    </noscript>
+
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        document.addEventListener("DOMContentLoaded", function () {
+            let hasLoaded = false;
+
+            function loadGTM() {
+                if (hasLoaded) return;
+                hasLoaded = true;
+
                 let script = document.createElement("script");
                 script.src = "https://www.googletagmanager.com/gtag/js?id=G-L98JG9ZT7H";
                 script.async = true;
                 document.head.appendChild(script);
 
-                script.onload = function() {
+                script.onload = function () {
                     gtag('js', new Date());
                     gtag('config', 'G-L98JG9ZT7H');
                 };
             }
-        }
 
-        window.addEventListener("scroll", loadGTM, {
-            once: true
+            ['scroll', 'mousemove', 'touchstart'].forEach(event =>
+                window.addEventListener(event, loadGTM, { once: true })
+            );
         });
-        window.addEventListener("mousemove", loadGTM, {
-            once: true
-        });
-        window.addEventListener("touchstart", loadGTM, {
-            once: true
-        });
-    });
-</script>
+    </script>
+</head>
