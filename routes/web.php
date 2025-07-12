d9e59848446c1aad917b9682064312cdcab3563e
ameeqkhan <ameeqkhan183@gmail.com>
2025-07-13 00:46:48 +0500

    sadasd

--- routes/web.php
+++ routes/web.php
@@ -36,21 +36,6 @@ Route::middleware('admin')->group(function () {
     Route::resource('orders', OrderController::class);
 });
 
-Route::get('/storage-link', function () {
-    $target = storage_path('app/public');
-    $link = public_path('storage');
-
-    if (file_exists($link)) {
-        return 'Link already exists.';
-    }
-
-    // Only works if shell_exec is enabled and safe_mode is off
-    shell_exec("ln -s $target $link");
-
-    return 'Storage link created using shell_exec.';
-});
-
-
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
