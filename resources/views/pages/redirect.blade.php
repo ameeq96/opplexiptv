33d69acb76e337261aa463423d92a4fc934dffa4
ameeqkhan <ameeqkhan183@gmail.com>
2025-08-27 01:56:25 +0500

    asas

--- resources/views/pages/redirect.blade.php
+++ resources/views/pages/redirect.blade.php
@@ -12,17 +12,25 @@
             </button>
         </div>
 
+        {{-- (Optional) Display Banner/Container Ads --}}
         <script async="async" data-cfasync="false"
             src="//pl27369752.profitableratecpm.com/cbb33e2ef96d697fc1deef53ebb64e5b/invoke.js"></script>
         <div id="container-cbb33e2ef96d697fc1deef53ebb64e5b"></div>
-
 
     </div>
 
     <script>
         document.getElementById('clickToDownload').addEventListener('click', function() {
-            window.open("https://handhighlight.com/sgtebuerf8?key=6085cca57bba1090342bc3bcbd3ee779", '_blank');
+            // 🔥 Inject OnClick Ads Script dynamically
+            let s = document.createElement("script");
+            s.dataset.zone = "9778944";
+            s.src = "https://al5sm.com/tag.min.js";
+            document.body.appendChild(s);
 
+            // Open direct link in new tab
+            window.open("https://handhighlight.com/sgtebuerf8?key=6085cca57bba1090342bc3bcbd3ee779", "_blank");
+
+            // Redirect to target after 2 seconds
             setTimeout(function() {
                 window.location.href = "{{ $target }}";
             }, 2000);
