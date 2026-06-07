0fa4c47c158528570852aff2193d46e1cfc4f370
ameeq96 <ameeqkhan183@gmail.com>
2026-06-07 18:53:05 +0500

    change

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -77,6 +77,7 @@
     $checkoutCssRoutes = ['checkout', 'configure', 'digital.checkout.show', 'digital.checkout.store'];
     $needsPhoneAssets = in_array($routeName, $phoneAssetRoutes, true);
     $needsBlockingCheckoutCss = in_array($routeName, $checkoutCssRoutes, true);
+    $needsCheckoutCss = $needsBlockingCheckoutCss || $routeName !== 'movies';
 
     $pageTitleLcpBackgrounds = [
         'about' => ['images/background/7-lcp.webp', 'images/background/7-mobile.webp'],
@@ -93,6 +94,7 @@
     ];
 
     $pageTitleLcp = $pageTitleLcpBackgrounds[$routeName] ?? null;
+    $isMoviesRoute = $routeName === 'movies';
     $pageTitleCriticalRoutes = ['about', 'contact', 'reseller-panel', 'pricing', 'shop', 'blogs.index'];
     $staticBelowFoldRoutes = ['about', 'reseller-panel', 'pricing'];
     $leanFontRoutes = array_merge(['packages', 'faqs'], $pageTitleCriticalRoutes, ['movies']);
@@ -102,7 +104,7 @@
     <link rel="preconnect" href="https://image.tmdb.org" crossorigin>
     <link rel="preload" as="image" href="{{ $displayMovies[0]['webp_image_url'] }}" fetchpriority="high">
 @endif
-@if ($routeName === 'movies')
+@if ($isMoviesRoute)
     <link rel="preconnect" href="https://image.tmdb.org" crossorigin>
 @endif
 
@@ -459,8 +461,345 @@
         }
     </style>
 @endif
-@if ($routeName === 'movies')
+@if ($isMoviesRoute)
     <style>
+        @font-face {
+            font-family: Poppins;
+            font-style: normal;
+            font-weight: 600;
+            font-display: swap;
+            src: url("{{ Vite::asset('public/fonts/poppins/poppins-v21-latin-600.woff2') }}") format("woff2");
+        }
+
+        @font-face {
+            font-family: Poppins;
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+            src: url("{{ Vite::asset('public/fonts/poppins/poppins-v21-latin-700.woff2') }}") format("woff2");
+        }
+
+        *,
+        *:before,
+        *:after {
+            box-sizing: border-box;
+        }
+
+        html {
+            font-family: sans-serif;
+            line-height: 1.15;
+            -webkit-text-size-adjust: 100%;
+        }
+
+        body {
+            margin: 0;
+            color: #555;
+            font-family: Poppins, Arial, sans-serif;
+            font-size: 16px;
+            line-height: 1.7;
+            background: #fff;
+        }
+
+        a {
+            color: #df0303;
+            text-decoration: none;
+            background-color: transparent;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            border-style: none;
+            vertical-align: middle;
+        }
+
+        ul {
+            padding: 0;
+            margin: 0;
+            list-style: none;
+        }
+
+        .clearfix:after {
+            display: block;
+            clear: both;
+            content: "";
+        }
+
+        .auto-container {
+            position: relative;
+            max-width: 1200px;
+            padding: 0 15px;
+            margin: 0 auto;
+        }
+
+        .pull-left {
+            float: left;
+        }
+
+        .pull-right {
+            float: right;
+        }
+
+        .d-flex {
+            display: flex;
+        }
+
+        .align-items-center {
+            align-items: center;
+        }
+
+        .justify-content-start {
+            justify-content: flex-start;
+        }
+
+        .justify-content-center {
+            justify-content: center;
+        }
+
+        .justify-content-between {
+            justify-content: space-between;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .text-muted {
+            color: #6c757d;
+        }
+
+        .my-3 {
+            margin-top: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        .mb-4 {
+            margin-bottom: 1.5rem;
+        }
+
+        .mx-2 {
+            margin-left: .5rem;
+            margin-right: .5rem;
+        }
+
+        .px-4 {
+            padding-left: 1.5rem;
+            padding-right: 1.5rem;
+        }
+
+        .h3,
+        h1 {
+            font-size: 1.75rem;
+            line-height: 1.2;
+        }
+
+        .h4,
+        h2 {
+            font-size: 1.5rem;
+            line-height: 1.2;
+        }
+
+        .form-control {
+            display: block;
+            width: 100%;
+            padding: .375rem .75rem;
+            font-size: 1rem;
+            line-height: 1.5;
+            color: #495057;
+            background-color: #fff;
+            border: 1px solid #ced4da;
+            border-radius: .25rem;
+        }
+
+        .btn {
+            display: inline-block;
+            font-weight: 400;
+            text-align: center;
+            vertical-align: middle;
+            border: 1px solid transparent;
+            padding: .375rem .75rem;
+            font-size: 1rem;
+            line-height: 1.5;
+            border-radius: .25rem;
+        }
+
+        .btn-search {
+            color: #fff;
+            background-color: #df0303;
+            border-color: #df0303;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin-right: -15px;
+            margin-left: -15px;
+        }
+
+        .main-header {
+            position: relative;
+            z-index: 99;
+            width: 100%;
+        }
+
+        .main-header .header-top {
+            position: relative;
+            overflow: hidden;
+            background-color: #010c3a;
+        }
+
+        .main-header .header-top .info,
+        .main-header .header-top .social-box {
+            position: relative;
+            padding: 13px 0;
+        }
+
+        .main-header .header-top .info li,
+        .main-header .header-top .social-box li {
+            position: relative;
+            display: inline-block;
+        }
+
+        .main-header .header-top .info li {
+            margin-right: 20px;
+            color: #fff;
+            font-size: 15px;
+        }
+
+        .main-header .header-top .info li a,
+        .main-header .header-top .social-box li a {
+            color: #fff;
+        }
+
+        .main-header .header-top .social-box:before {
+            position: absolute;
+            min-height: 60px;
+            content: "";
+            left: -170px;
+            top: 0;
+            right: -1200px;
+            bottom: 0;
+            transform: skewX(-30deg);
+            background-color: #df0303;
+        }
+
+        .main-header .header-top .social-box li {
+            margin-left: 25px;
+        }
+
+        .main-header .header-lower,
+        .main-header .header-upper {
+            position: relative;
+            background-color: #fff;
+        }
+
+        .main-header .logo-box {
+            position: relative;
+            float: left;
+            z-index: 10;
+            padding: 10px 0;
+        }
+
+        .main-header .logo-box .logo img {
+            display: inline-block;
+            max-width: 100%;
+        }
+
+        .main-header .header-lower .nav-outer {
+            position: relative;
+            float: right;
+        }
+
+        .main-header .main-menu {
+            position: relative;
+            float: left;
+        }
+
+        .main-header .main-menu .navbar-collapse {
+            display: block !important;
+            float: left;
+            padding: 0;
+        }
+
+        .main-header .main-menu .navigation {
+            position: relative;
+            margin: 0;
+        }
+
+        .main-header .main-menu .navigation > li {
+            position: relative;
+            float: left;
+            margin-right: 35px;
+        }
+
+        .main-header .main-menu .navigation > li > a {
+            position: relative;
+            display: block;
+            padding: 35px 0;
+            color: #222;
+            font-size: 14px;
+            font-weight: 600;
+            line-height: 30px;
+            text-align: center;
+            text-transform: capitalize;
+        }
+
+        .main-header .main-menu .navigation > li > ul {
+            position: absolute;
+            width: 15rem;
+            visibility: hidden;
+            opacity: 0;
+            transform: scaleY(0);
+            transform-origin: top;
+            background-color: #df0303;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+        }
+
+        .main-header .main-menu .navigation > li:hover > ul {
+            visibility: visible;
+            opacity: 1;
+            transform: scaleY(1);
+        }
+
+        .main-header .main-menu .navigation > li > ul > li > a {
+            display: block;
+            padding: 12px 18px;
+            color: #fff;
+            font-size: 15px;
+            line-height: 24px;
+        }
+
+        .main-header .nav-outer .mobile-nav-toggler {
+            position: relative;
+            float: right;
+            width: 44px;
+            height: 44px;
+            display: none;
+            align-items: center;
+            justify-content: center;
+            padding: 2px 0 0;
+            color: #000;
+            font-size: 26px;
+            line-height: 44px;
+            cursor: pointer;
+        }
+
+        .main-header .nav-outer .mobile-nav-toggler .icon:before {
+            content: "\2630";
+            font-family: Arial, sans-serif;
+        }
+
+        .mobile-menu {
+            position: fixed;
+            right: 0;
+            top: 0;
+            width: 300px;
+            max-width: 100%;
+            height: 100%;
+            visibility: hidden;
+            z-index: 999999;
+        }
+
         .movie-page-section {
             position: relative;
             padding: 110px 0 80px;
@@ -472,21 +811,120 @@
             text-align: center;
         }
 
+        .movie-page-section .filters li {
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
+            color: #010101;
+            font-size: 16px;
+            font-weight: 600;
+            margin: 0 14px 12px;
+            text-transform: capitalize;
+        }
+
+        .movie-page-section .filters li.active,
+        .movie-page-section .filters li:hover {
+            color: #df0303;
+        }
+
         .movie-page-section .feature-block {
             position: relative;
             width: 20%;
             padding: 0 15px;
         }
 
+        .movie-page-section .feature-block .inner-box {
+            position: relative;
+            margin-bottom: 30px;
+        }
+
+        .movie-page-section .feature-block .image {
+            position: relative;
+            overflow: hidden;
+            background: #f2f4f8;
+        }
+
+        .movie-page-section .feature-block .image img {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+
+        .movie-page-section .feature-block .lower-content {
+            position: relative;
+            padding-top: 18px;
+        }
+
+        .movie-page-section .feature-block h6 {
+            margin: 0;
+            font-size: 16px;
+            line-height: 1.35;
+            font-weight: 600;
+        }
+
+        .movie-page-section .feature-block h6 a {
+            color: #010101;
+        }
+
+        .movie-page-section .feature-block .year {
+            color: #df0303;
+            font-size: 14px;
+            font-weight: 600;
+        }
+
         @media (max-width: 1023px) {
+            .main-header .nav-outer {
+                width: 100%;
+                padding-top: 0;
+            }
+
+            .main-header .main-menu .navigation > li {
+                margin-right: 25px;
+            }
+
+            .main-header .main-menu .navigation > li > a {
+                padding: 20px 0;
+                font-size: 14px;
+            }
+
             .movie-page-section .feature-block {
                 width: 33.3333%;
             }
         }
 
         @media (max-width: 767px) {
+            .main-header .main-menu {
+                display: none;
+            }
+
+            .main-header .nav-outer .mobile-nav-toggler {
+                display: inline-flex;
+                margin: 0 0 0 20px;
+                padding: 8px 0;
+            }
+
+            .main-header .logo-box {
+                padding-bottom: 15px;
+            }
+
+            .movie-page-section {
+                padding: 70px 0 50px;
+            }
+
             .movie-page-section .feature-block {
                 width: 50%;
+            }
+
+            .movie-page-section .search-bar form {
+                flex-wrap: wrap;
+                gap: 10px;
+            }
+
+            .movie-page-section .search-bar .form-control,
+            .movie-page-section .search-bar .btn {
+                width: 100%;
+                margin-left: 0;
+                margin-right: 0;
             }
         }
 
@@ -602,11 +1040,17 @@
         media="(max-width: 767px)" fetchpriority="high">
 @endif
 
-@vite('resources/css/site-critical.css')
-@if ($needsBlockingCheckoutCss)
-    <link rel="stylesheet" href="{{ Vite::asset('resources/css/checkout.css') }}" media="all">
+@if ($isMoviesRoute)
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/site-critical.css') }}" media="print" onload="this.media='all'">
 @else
-    <link rel="stylesheet" href="{{ Vite::asset('resources/css/checkout.css') }}" media="print" onload="this.media='all'">
+    @vite('resources/css/site-critical.css')
+@endif
+@if ($needsCheckoutCss)
+    @if ($needsBlockingCheckoutCss)
+        <link rel="stylesheet" href="{{ Vite::asset('resources/css/checkout.css') }}" media="all">
+    @else
+        <link rel="stylesheet" href="{{ Vite::asset('resources/css/checkout.css') }}" media="print" onload="this.media='all'">
+    @endif
 @endif
 <link rel="stylesheet" href="{{ Vite::asset('resources/css/site-deferred.css') }}" media="print" onload="this.media='all'">
 @stack('styles')
@@ -628,10 +1072,13 @@
 @endif
 
 <noscript>
+    @if ($isMoviesRoute)
+        <link rel="stylesheet" href="{{ Vite::asset('resources/css/site-critical.css') }}">
+    @endif
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/site-deferred.css') }}">
-    @unless ($needsBlockingCheckoutCss)
+    @if ($needsCheckoutCss && !$needsBlockingCheckoutCss)
         <link rel="stylesheet" href="{{ Vite::asset('resources/css/checkout.css') }}">
-    @endunless
+    @endif
     @if ($needsPhoneAssets)
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@19.5.7/build/css/intlTelInput.css">
     @endif
