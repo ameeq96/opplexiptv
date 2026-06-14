33f7dc508c8a8e914a28797e8c61de4cc165301f
ameeq96 <ameeqkhan183@gmail.com>
2026-06-14 22:42:04 +0500

    change

--- resources/views/includes/_home-feature-split.blade.php
+++ resources/views/includes/_home-feature-split.blade.php
@@ -3,8 +3,9 @@
     <div class="auto-container">
         <div class="row align-items-center">
             <div class="col-lg-6 mb-4 mb-lg-0">
+                {{-- LCP image on home: must load eagerly with high priority (no loading=lazy) --}}
                 <img src="{{ asset('images/resource/streaming-3.webp') }}"
-                    alt="{{ __('messages.home_split_image_alt') }}" loading="lazy" decoding="async"
+                    alt="{{ __('messages.home_split_image_alt') }}" fetchpriority="high" decoding="async"
                     width="390" height="475" class="img-fluid" style="width: 100%; height: auto;" />
             </div>
             <div class="col-lg-6" style="text-align: {{ $isRtl ? 'right' : 'left' }};">
